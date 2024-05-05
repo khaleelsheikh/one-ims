9792226046d39ea38f9b890b1daad4d945570293
khaleelsheikh <khaleels.workmail@gmail.com>
2024-05-05 11:30:34 +0300

    Hot reload is not working properly. Not showing changes. Requires to restart the server.

--- app/dashboard/Dashboard.jsx
+++ app/dashboard/Dashboard.jsx
@@ -2,15 +2,16 @@
 import React from "react";
 import SideNavbar from "@/app/components/SideNavbar";
 
-
-
 const Dashboard = () => {
+  let username = "Khaleel R Sheikh";
   return (
     <>
-       <div className="flex">
-          <SideNavbar/>
-          <div className="flex-grow p-4 h-[96vh]">This is DashBoard</div>
+      <div className="flex">
+        <SideNavbar />
+        <div className="flex-grow p-4 h-[96vh]">
+          <h1>Welcome {username}</h1>
         </div>
+      </div>
     </>
   );
 };
