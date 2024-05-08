d324273a7aa4b1a4737fdadcf3fe4c6dacb5980b
khaleelsheikh <khaleels.workmail@gmail.com>
2024-05-08 11:44:10 +0300

    Removed all supabase packages. Need to try something new to interact with supabase.

---
+++ app/login/page.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Login from "./Login";
+import SideNavbar from "../components/SideNavbar";
+
+export const metadata = {
+  title: "One: Login",
+  description: "Spare Parts Inventory Management System",
+};
+
+const LoginPage = () => {
+  return (
+    <>
+      <Login />
+    </>
+  );
+};
+
+export default LoginPage;
---
+++ app/users/add/page.js
@@ -0,0 +1,17 @@
+import React from "react";
+import AddUser from "./AddUser";
+
+export const metadata = {
+  title: "One: Add User",
+  description: "Spare Parts Inventory Management System",
+};
+
+const AddUserPage = () => {
+  return (
+    <>
+      <AddUser />
+    </>
+  );
+};
+
+export default AddUserPage;
